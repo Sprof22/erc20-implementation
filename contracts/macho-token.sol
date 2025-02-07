f4517b80985b2224071df3f83fbfa56af39d9718
Sprof22 <richmondelaigwu@gmail.com>
2025-02-07 08:15:54 +0100

    macho token implementation  without zeppelin

--- contracts/macho-token.sol
+++ contracts/macho-token.sol
@@ -1,73 +1,48 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.26;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
-contract ERC20Standard {
-    
+contract MachoToken {
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
-    // State variables
-    string public name; 
-    string public symbol; 
-    uint8 public decimals; 
-    uint256 public totalSupply; 
+    string public name = "Macho Token"; // Name of the token
+    string public symbol = "MCT"; // Symbol of the token
+    uint8 public decimals = 18; // Number of decimal places
+    uint256 public totalSupply = 1_000_000 * (10**uint256(decimals)); // Total supply: 1,000,000 MCT
 
     mapping(address => uint256) private _balances;
     mapping(address => mapping(address => uint256)) private _allowances;
 
-    
-    constructor(
-        string memory _name, 
-        string memory _symbol, 
-        uint256 _totalSupply
-        ) {
-        name = _name;
-        symbol = _symbol;
-        decimals = 18; 
-        totalSupply = _totalSupply * (10**uint256(decimals)); 
-        _balances[msg.sender] = totalSupply; 
-        emit Transfer(address(0), msg.sender, totalSupply); 
+    constructor() {
+        _balances[msg.sender] = totalSupply; // Assign all tokens to the contract deployer
+        emit Transfer(address(0), msg.sender, totalSupply); // Minting event
     }
 
-   
     function balanceOf(address account) public view returns (uint256) {
         return _balances[account];
     }
 
-   
     function transfer(address recipient, uint256 amount) public returns (bool) {
         _transfer(msg.sender, recipient, amount);
         return true;
     }
 
-    
     function approve(address spender, uint256 amount) public returns (bool) {
         _approve(msg.sender, spender, amount);
         return true;
     }
 
-   
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) public returns (bool) {
+    function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
         _spendAllowance(sender, msg.sender, amount);
         _transfer(sender, recipient, amount);
         return true;
     }
 
-   
     function allowance(address owner, address spender) public view returns (uint256) {
         return _allowances[owner][spender];
     }
 
-    
-    function _transfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal {
+    function _transfer(address from, address to, uint256 amount) internal {
         require(from != address(0), "ERC20: transfer from the zero address");
         require(to != address(0), "ERC20: transfer to the zero address");
         require(_balances[from] >= amount, "ERC20: insufficient balance");
@@ -78,12 +53,7 @@ contract ERC20Standard {
         emit Transfer(from, to, amount);
     }
 
-    
-    function _approve(
-        address owner,
-        address spender,
-        uint256 amount
-    ) internal {
+    function _approve(address owner, address spender, uint256 amount) internal {
         require(owner != address(0), "ERC20: approve from the zero address");
         require(spender != address(0), "ERC20: approve to the zero address");
 
@@ -92,12 +62,7 @@ contract ERC20Standard {
         emit Approval(owner, spender, amount);
     }
 
-    
-    function _spendAllowance(
-        address owner,
-        address spender,
-        uint256 amount
-    ) internal {
+    function _spendAllowance(address owner, address spender, uint256 amount) internal {
         uint256 currentAllowance = _allowances[owner][spender];
         require(currentAllowance >= amount, "ERC20: insufficient allowance");
         _approve(owner, spender, currentAllowance - amount);
